22003f3f0ed2e9c53f455648c918a11cbf15eb9e
dpranata3 <donny.pranata3@gmail.com>
2019-08-03 15:31:47 +0700

    initial commit

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, Text, Image, Linking } from "react-native";
+
+import Card from "./Card";
+import CardSection from "./CardSection";
+import Button from './Button';
+
+// style album detail
+
+const styles={
+    headerContentStyle:{
+        justifyContent:'space-around'
+    },
+    headerTextStyle:{
+        fontSize:18
+    },
+    thumbnailContainerStyle:{
+        justifyContent:'center',
+        alignItems:'center',
+        marginLeft:10,
+        marginRigh:10
+    },
+    thumbnailStyle:{
+        height:50,
+        width:50
+    },
+    imageStyle:{
+        height:300,
+        flex:1
+    }
+}
+
+const AlbumDetail =({album})=>{
+    const {
+        headerContentStyle,
+        headerTextStyle,
+        thumbnailContainerStyle,
+        thumbnailStyle,
+        imageStyle
+    } = styles
+
+
+    const {
+        title,
+        artist,
+        thumbnail_image,
+        image,
+        url
+    }  = album
+
+    return (
+        <Card>           
+          <CardSection>
+            <View style={thumbnailContainerStyle}>
+                <Image 
+                   style={thumbnailStyle}
+                   source={{uri: thumbnail_image}}
+                />
+            </View>
+            <View style={headerContentStyle}>
+                <Text style={headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+          </CardSection>
+          <CardSection>
+              <Image 
+                style={imageStyle}
+                source={{uri:image}}
+              />
+          </CardSection>
+          <CardSection>
+              <Button onPress={()=> Linking.openURL(url)}>
+                  Buy Now
+              </Button>
+          </CardSection>
+        </Card>
+      );
+};
+
+export default AlbumDetail;
